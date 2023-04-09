437b714297a1d6eb3ed578d3529af7fdafe8f518
30334243 <fabish@bk.ru>
2023-04-09 22:20:02 +0300

    minor changes

--- session.hpp
+++ session.hpp
@@ -20,14 +20,14 @@ template<class Type = bool>
 				auto GetConversationId(Tail&&... tail) {
 					std::tuple<size_t, bool> ret{};
 					auto [key_first, key_second] = transform(std::forward<Tail>(tail)...);
-					if (auto it{m_sessions.find(key_first)}; it != m_sessions.cend()) {
+					if (auto it{this->sessions.find(key_first)}; it != this->sessions.cend()) {
 						ret = std::make_tuple(VectorHasher()(it->first), it->second);
-					} else if (auto it{m_sessions.find(key_second)};
-								  it != m_sessions.cend()) {
+					} else if (auto it{this->sessions.find(key_second)};
+								  it != this->sessions.cend()) {
 						ret = std::make_tuple(VectorHasher()(it->first), it->second);
 					} else {
-						m_sessions[key_first] = false;
-						m_sessions[key_second] = true;
+						this->sessions[key_first] = false;
+						this->sessions[key_second] = true;
 						ret = std::make_tuple(VectorHasher()(key_first), false);
 					}
 					return ret;
@@ -40,17 +40,17 @@ template<class Type = bool>
 							std::tuple<size_t, bool> ret{};
 							auto [key_first, key_second] =
 								transform(std::forward<decltype(ts)>(ts)...);
-							if (auto it{m_sessions.find(key_first)};
-								 it != m_sessions.cend()) {
+							if (auto it{this->sessions.find(key_first)};
+								 it != this->sessions.cend()) {
 								write(it->second);
-							} else if (auto it{m_sessions.find(key_second)};
-										  it != m_sessions.cend()) {
+							} else if (auto it{this->sessions.find(key_second)};
+										  it != this->sessions.cend()) {
 								write(it->second);
 							} else {
 								auto new_dst{
 									create(std::to_string(VectorHasher()(key_first)))};
-								m_sessions[key_first] = new_dst;
-								m_sessions[key_second] = new_dst;
+								this->sessions[key_first] = new_dst;
+								this->sessions[key_second] = new_dst;
 							}
 						};
 					};
@@ -58,79 +58,79 @@ template<class Type = bool>
 			// GET MAP
 			std::unordered_map<std::vector<uint8_t>, Type, VectorHasher> const&
 				GetMap() const {
-					return m_sessions;
+					return this->sessions;
 				}
 		// PRIVATE
 		private:
 			// INTEGRALL TO VECTOR
-			template<int IdxF, int IdxS, class T,
+			template<int F, int S, class T,
 				typename = std::enable_if_t<std::is_integral_v<T>>>
-					static auto integral_to_vec(Elm<T>& var) {
+					static auto integralToVec(Elm<T>& var) {
 						std::vector<uint8_t> ret(sizeof(T) * 2);
-						std::copy((uint8_t*)&std::get<IdxF>(var),
-									 (uint8_t*)&std::get<IdxF>(var) + sizeof(T),
+						std::copy((uint8_t*)&std::get<F>(var),
+									 (uint8_t*)&std::get<F>(var) + sizeof(T),
 									 ret.begin());
-						std::copy((uint8_t*)&std::get<IdxS>(var),
-									 (uint8_t*)&std::get<IdxS>(var) + sizeof(T),
+						std::copy((uint8_t*)&std::get<S>(var),
+									 (uint8_t*)&std::get<S>(var) + sizeof(T),
 									 ret.begin() + sizeof(T));
 						return ret;
 					}
 			// INTEGRALL TO VECTOR
-			template<int IdxF, int IdxS, class T,
+			template<int F, int S, class T,
 				typename = std::enable_if_t<std::is_integral_v<T>>>
-					static constexpr auto integral_to_vec() {
+					static constexpr auto integralToVec() {
 						return [] (Elm<T>& var) {
-							return integral_to_vec<IdxF, IdxS, T>(var);
+							return integralToVec<F,S,T>(var);
 						};
 					}
 			// STRING TO VECTOR
-			template<int IdxF, int IdxS>
-				static auto str_to_vec(Elm<std::string>& str) {
+			template<int F, int S>
+				static auto strToVec(Elm<std::string>& str) {
 					std::vector<uint8_t> ret(str.first.size() + str.second.size());
-					std::copy(std::get<IdxF>(str).data(),
-								 std::get<IdxF>(str).data() + std::get<IdxF>(str).size(),
+					std::copy(std::get<F>(str).data(),
+								 std::get<F>(str).data() + std::get<F>(str).size(),
 								 ret.begin());
-					std::copy(std::get<IdxS>(str).data(),
-								 std::get<IdxS>(str).data() + std::get<IdxS>(str).size(),
-								 ret.begin() + std::get<IdxF>(str).size());
+					std::copy(std::get<S>(str).data(),
+								 std::get<S>(str).data() + std::get<S>(str).size(),
+								 ret.begin() + std::get<F>(str).size());
 					return ret;
 				}
 			// STRING TO VECTOR
-			template<int IdxF, int IdxS>
-				static auto str_to_vec() {
+			template<int F, int S>
+				static auto strToVec() {
 					return [] (Elm<std::string>& str) {
-						return str_to_vec<IdxF, IdxS>(str);
+						return strToVec<F,S>(str);
 					};
 				}
 			// POINTER TO VECTOR
-			template<int IdxF, int IdxS, class T,
+			template<int F, int S, class T,
 				typename = std::enable_if_t<std::is_pointer_v<T>>>
-					static auto ptr_to_vec(Ptr<T>& ptr) {
+					static auto ptrToVec(Ptr<T>& ptr) {
 						std::vector<uint8_t> ret(ptr.first.second + ptr.second.second);
-						std::copy(std::get<IdxF>(ptr).first,
-									 std::get<IdxF>(ptr).first + std::get<IdxF>(ptr).second,
+						std::copy(std::get<F>(ptr).first,
+									 std::get<F>(ptr).first + std::get<F>(ptr).second,
 									 ret.begin());
-						std::copy(std::get<IdxS>(ptr).first,
-									 std::get<IdxS>(ptr).first + std::get<IdxS>(ptr).second,
-									 ret.begin() + std::get<IdxF>(ptr).second);
+						std::copy(std::get<S>(ptr).first,
+									 std::get<S>(ptr).first + std::get<S>(ptr).second,
+									 ret.begin() + std::get<F>(ptr).second);
 						return ret;
 					}
 			// POINTER TO VECTOR
-			template<int IdxF, int IdxS, class T,
+			template<int F, int S, class T,
 				typename = std::enable_if_t<std::is_pointer_v<T>>>
-					static auto ptr_to_vec() {
+					static auto ptrToVec() {
 						return [] (Ptr<T>& ptr) {
-							return ptr_to_vec<IdxF, IdxS, T>(ptr);
+							return ptrToVec<F,S,T>(ptr);
 						};
 					}
 			// SWAP
 			template<int F, int S>
 				static auto swap(var_t& val) {
 					return std::visit(overloaded{
-						integral_to_vec<F, S, int>(),
-						integral_to_vec<F, S, char>(),
-						ptr_to_vec<F, S, uint8_t*>(),
-						str_to_vec<F, S>(),
+						integralToVec<F,S,int>(),
+						integralToVec<F,S,char>(),
+						ptrToVec<F,S,uint8_t*>(),
+						strToVec<F,S>(),
 					}, val);
 				}
 			// TRNASFORM
@@ -147,10 +147,12 @@ template<class Type = bool>
 			template<class... Ts>
 				auto transform(Ts&&... ts) {
 					ret_t vec{std::forward<Ts>(ts)...};
-					return std::make_tuple(transform<0,1>(vec), transform<1,0>(vec));
+					std::vector<uint8_t> original{transform<0,1>(vec)};
+					std::vector<uint8_t> reverse{transform<1,0>(vec)};
+					return std::make_tuple(original, reverse);
 				}
 			// SESSIONS
-			std::unordered_map<std::vector<uint8_t>, Type, VectorHasher> m_sessions{};
+			std::unordered_map<std::vector<uint8_t>, Type, VectorHasher> sessions{};
 	};
 }
 #endif
